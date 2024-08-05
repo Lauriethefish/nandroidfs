fd15d9ca36c37d405aa8a25caa0e00e6a6a73b4a
Lauriethefish <44222319+Lauriethefish@users.noreply.github.com>
2024-08-05 08:52:08 +0200

    Prevent multiple instances from being open at once

--- nandroidfs/main.cpp
+++ nandroidfs/main.cpp
@@ -26,12 +26,37 @@ void scan_for_devices(ContextLogger& logger) {
 	logger.info("got Ctrl + C signal, shutting down active filesystems");
 }
 
+// Checks if NandroidFS is already running.
+// If it is, a message box is shown to the user informing them of this, and the function returns `false`.
+// If it is not, this function returns `true`.
+bool prompt_if_already_running() {
+	CreateMutex(NULL, true, L"NandroidFS_Mutex");
+	switch (GetLastError()) {
+	case ERROR_SUCCESS:
+		return true;
+	case ERROR_ALREADY_EXISTS:
+		MessageBox(NULL, L"NandroidFS is already running. Only one instance is allowed at any one time."
+			"\nPlease exit the existing instance if you wish to restart the program.",
+			L"NandroidFS already open",
+			MB_ICONWARNING | MB_OK);
+		return false;
+	default:
+		// Unknown error occured. Return false to prevent the program from launching (since we're not sure if it's running already)
+		return false;
+	}
+}
+
 
 int CALLBACK WinMain(HINSTANCE hInstance,
 	HINSTANCE hPrevInstance,
 	LPSTR lpCmdLine,
 	int cCmdShow)
 {
+	// Check that we don't have two instances running at once.
+	if (!prompt_if_already_running()) {
+		return 1;
+	}
+
 	ContextLogger logger(std::make_shared<StdOutSink>(), "main", LogLevel::Trace);
 	std::ios_base::sync_with_stdio(false); // Speed up logging by decoupling C and C++ IO.
 
